8c1a0354c8f074ee6ae63c3c1c6c308033240b55
André Teixeira <algteixeira@inf.ufpel.edu.br>
2021-10-28 11:39:15 -0300

    Created a query validator for cars GET route

---
+++ src/app/validation/car/carQueryValidation.js
@@ -0,0 +1,21 @@
+const Joi = require('joi');
+
+module.exports = async (req, res, next) => {  
+  try {
+    const schema = Joi.object({
+      limit: Joi.number(),
+      offset: Joi.number(),
+      page: Joi.number(),
+      modelo: Joi.string(),
+      cor: Joi.string(),
+      ano: Joi.number().min(1950).max(2022),
+      acessorios: Joi.array().items({descricao: Joi.string()}),
+      quantidadePassageiros: Joi.number()
+    });
+    const { error } = await schema.validate(req.query, { abortEarly: false });
+    if (error) throw error
+    return next();
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+}
--- src/routes/car.router.js
+++ src/routes/car.router.js
@@ -1,10 +1,11 @@
 const CarController = require('../app/controller/CarController');
 const createCarValidation = require('../app/validation/car/create');
 const updateCarValidation = require('../app/validation/car/updateCarValidation');
+const carQueryValidation = require('../app/validation/car/carQueryValidation');
 
 module.exports = (server, routes, prefix = '/car') => {
   routes.post('/', createCarValidation, CarController.create);
-  routes.get('/', CarController.find);
+  routes.get('/', carQueryValidation ,CarController.find);
   routes.get('/:id', CarController.findById);
   routes.delete('/:id', CarController.deleteCar);
   routes.put('/:id', updateCarValidation, CarController.update);
