9f1a78f30a2f678f6d527a4b43793962b2c3e4a6
André Teixeira <algteixeira@inf.ufpel.edu.br>
2021-11-04 00:33:04 -0300

    Joi objectId removed

--- src/app/validation/car/validateId.js
+++ src/app/validation/car/validateId.js
@@ -1,10 +1,10 @@
 const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
+
 
 module.exports = async (req, res, next) => {  
   try {
     const schema = Joi.object({
-      id: Joi.objectId().required()
+      id: Joi.string().regex(/[0-9a-fA-F]{24}/).length(24).required()
     });
     const { error } = await schema.validate(req.params, { abortEarly: false });
     if (error) throw error
