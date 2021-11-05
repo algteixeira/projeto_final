2c2c9c47a597f5568916433781ed48b2468eb02d
André Teixeira <algteixeira@inf.ufpel.edu.br>
2021-11-05 18:07:56 -0300

    Added on server a validation for the amount of filials in a Rental object

--- src/app/service/RentalService.js
+++ src/app/service/RentalService.js
@@ -1,13 +1,28 @@
 const RentalRepository = require("../repository/RentalRepository");
 const AlreadyExists = require("../errors/alreadyExists");
+const BadRequest = require("../errors/badRequest");
 
 class RentalService {
     async create (payload) {
+        let count = payload.endereco.filter((item) => item.isFilial===false);
+        if (count.length === 0 || count.length > 1) {
+            if (count.length === 0 ) {
+                console.log('Apenas filiais');
+            }
+            else {
+                console.log('Mais de uma matriz');
+            }
+            throw new BadRequest();
+        }
+
+
+    
         const findByName = await RentalRepository.findByName(payload.nome);
-        if (findByName) {
-            
+        if (findByName) {            
             throw new AlreadyExists();
         }
+        
+
         await RentalRepository.create(payload);
         return payload;
     }
