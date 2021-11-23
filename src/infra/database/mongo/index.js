e1d7025ab66b22902950853e9c9e5878fc928e95
André <algteixeira@inf.ufpel.edu.br>
2021-11-23 16:06:03 -0300

    adding enviroment vars for heroku

--- src/infra/database/mongo/index.js
+++ src/infra/database/mongo/index.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
+const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/projetofinal';
+
 class Database {
   constructor() {
     this.connect();
   }
 
   connect() {
-    return mongoose.connect(
-      'mongodb+srv://admin:admin@cluster0.u2oi9.mongodb.net/projetofinal?retryWrites=true&w=majority'
-    );
+    return mongoose.connect(uri);
   }
 }
 
