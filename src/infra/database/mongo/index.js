2ace6e88e637c24e1af270aa319f24d0b2823a65
André <algteixeira@inf.ufpel.edu.br>
2021-11-23 15:25:00 -0300

    Changing heroku execution path

--- src/infra/database/mongo/index.js
+++ src/infra/database/mongo/index.js
@@ -6,8 +6,12 @@ class Database {
   }
 
   connect() {
-    return mongoose.connect('mongodb://localhost:27017/projetofinal');
+    return mongoose.connect(
+      'mongodb+srv://admin:admin@cluster0.u2oi9.mongodb.net/projetofinal?retryWrites=true&w=majority'
+    );
   }
 }
 
 module.exports = new Database().connect();
+
+// mongodb://localhost:27017/projetofinal
